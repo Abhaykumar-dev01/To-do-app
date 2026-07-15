f918589be2785abca04303796bb09056c20a5d47
Abhay kumar <abhaykumardev001@gmail.com>
2026-07-16 00:34:56 +0530

    Set up signup route

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const app = express();
-const user = [];
-const organizatios = [];
-const boards = [];
-const issues = [];
+
+
+let USERS_ID = 1;
+let ORGANIZATION_ID = 1;
+let BOARD_ID = 1;
+let ISSUES_ID = 1;
+
+
+const USERS = [];
+const ORGANIZATIONS = [];
+const BOARDS = [];
+const ISSUES = [];
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("hii")
 })
 app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    const userExists = USERS.find(u => u.username === username);
+    if (userExists) {
+        res.status(411).json({
+            message: "user with this usrname already exists"
+
+        })
+        return;
+    }
+
+    USERS.push({
+        username,
+        password,
+        id: USERS_ID++
+    })
+    res.json({
+        message: "You have signed up successfully"
+    })
 })
 app.post("/signin", (req, res) => {
 

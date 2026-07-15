443ed93dbe288647382a602fff2c9d7ab9f7ba7c
Abhay kumar <abhaykumardev001@gmail.com>
2026-07-16 00:06:31 +0530

    Listed the required get and post routes

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,39 @@ const issues = [];
 app.get("/", (req, res) => {
     res.send("hii")
 })
+app.post("/signup", (req, res) => {
 
+})
+app.post("/signin", (req, res) => {
 
+})
+app.post("/organization", (req, res) => {
+
+})
+app.post("/add-memeber-to-organization", (req, res) => {
+
+})
+
+app.post("/board", (req, res) => {
+
+})
+app.post("/issue", (req, res) => {
+
+})
+
+app.get("/boards", (req, res) => {
+
+})
+app.get("/issues", (req, res) => {
+
+})
+app.get("/members", (req, res) => {
+
+})
+
+app.put("/issues", (req, res) => {
+
+})
 
 app.listen(3000, () => {
 
